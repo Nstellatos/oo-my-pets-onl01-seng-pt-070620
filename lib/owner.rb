e212c34e37b8cdc5e2ee06f8b809eb787e190710
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-07 00:59:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,7 +46,7 @@ def sell_pets
   end
 end
 def list_pets
-  "I have #{owner.dogs.count} dog(s), and #{owner.cats.count} cat(s)."
+  "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
 end
     
 
